04e278c1a2ad24ed82ff7181234887f4aea02274
IlMeloIl <jgvmelo123@gmail.com>
2025-07-08 20:52:28 -0300

    feat(logbook): Develop routine management view (add/remove/reorder exercises)

--- logbook/urls.py
+++ logbook/urls.py
@@ -6,8 +6,14 @@ app_name = 'logbook'
 urlpatterns = [
     path('', views.RoutineListView.as_view(), name='routine_list'),
     path('rotinas/add/', views.RoutineCreateView.as_view(), name='routine_add'),
+    path('rotinas/<int:pk>/', views.RoutineDetailView.as_view(), name='routine_detail'),
     path('rotinas/<int:pk>/edit/', views.RoutineUpdateView.as_view(), name='routine_edit'),
     path('rotinas/<int:pk>/delete/', views.RoutineDeleteView.as_view(), name='routine_delete'),
+    
+    path('rotinas/<int:routine_id>/add-exercise/', views.AddExerciseToRoutineView.as_view(), name='add_exercise_to_routine'),
+    path('rotinas/<int:routine_id>/remove-exercise/<int:exercise_id>/', views.RemoveExerciseFromRoutineView.as_view(), name='remove_exercise_from_routine'),
+    path('rotinas/<int:routine_id>/reorder/', views.ReorderExercisesView.as_view(), name='reorder_exercises'),
+    
     path('exercicios/', views.ExerciseListView.as_view(), name='exercise_list'),
     path('exercicios/add/', views.ExerciseCreateView.as_view(), name='exercise_add'),
     path('exercicios/<int:pk>/edit/', views.ExerciseUpdateView.as_view(), name='exercise_edit'),
--- logbook/views.py
+++ logbook/views.py
@@ -1,10 +1,14 @@
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from django.db import models
 from .models import Routine, Exercise, RoutineExercise
 from .forms import RoutineForm, ExerciseForm, RoutineExerciseForm
+from django.contrib import messages
+from django.http import JsonResponse
+from django.views import View
+from django.db import models, transaction
 
 class RoutineListView(LoginRequiredMixin, ListView):
     model = Routine
@@ -77,3 +81,82 @@ class ExerciseDeleteView(LoginRequiredMixin, DeleteView):
     
     def get_queryset(self):
         return Exercise.objects.filter(user=self.request.user)
+
+class RoutineDetailView(LoginRequiredMixin, DetailView):
+    model = Routine
+    template_name = 'logbook/routine_detail.html'
+    context_object_name = 'routine'
+    
+    def get_queryset(self):
+        return Routine.objects.filter(user=self.request.user)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['routine_exercises'] = self.object.routine_exercises.all()
+        context['form'] = RoutineExerciseForm(user=self.request.user)
+        return context
+
+class AddExerciseToRoutineView(LoginRequiredMixin, View):
+    def post(self, request, routine_id):
+        routine = get_object_or_404(Routine, id=routine_id, user=request.user)
+        form = RoutineExerciseForm(request.POST, user=request.user)
+        
+        if form.is_valid():
+            # Verificar se o exercício já está na rotina
+            exercise = form.cleaned_data['exercise']
+            if RoutineExercise.objects.filter(routine=routine, exercise=exercise).exists():
+                messages.error(request, f'O exercício "{exercise.name}" já está nesta rotina.')
+            else:
+                # Definir a ordem como o próximo número
+                max_order = RoutineExercise.objects.filter(routine=routine).aggregate(
+                    models.Max('order')
+                )['order__max'] or 0
+                
+                routine_exercise = form.save(commit=False)
+                routine_exercise.routine = routine
+                routine_exercise.order = max_order + 1
+                routine_exercise.save()
+                messages.success(request, f'Exercício "{exercise.name}" adicionado à rotina.')
+        else:
+            messages.error(request, 'Erro ao adicionar exercício. Verifique os dados.')
+        
+        return redirect('logbook:routine_detail', pk=routine_id)
+
+class RemoveExerciseFromRoutineView(LoginRequiredMixin, View):
+    def post(self, request, routine_id, exercise_id):
+        routine = get_object_or_404(Routine, id=routine_id, user=request.user)
+        routine_exercise = get_object_or_404(
+            RoutineExercise, 
+            routine=routine, 
+            exercise_id=exercise_id
+        )
+        
+        exercise_name = routine_exercise.exercise.name
+        routine_exercise.delete()
+        
+        # Reordenar os exercícios restantes
+        remaining_exercises = RoutineExercise.objects.filter(routine=routine).order_by('order')
+        for i, re in enumerate(remaining_exercises, 1):
+            re.order = i
+            re.save()
+        
+        messages.success(request, f'Exercício "{exercise_name}" removido da rotina.')
+        return redirect('logbook:routine_detail', pk=routine_id)
+
+class ReorderExercisesView(LoginRequiredMixin, View):
+    def post(self, request, routine_id):
+        routine = get_object_or_404(Routine, id=routine_id, user=request.user)
+        
+        try:
+            exercise_ids = request.POST.getlist('exercise_ids[]')
+            
+            with transaction.atomic():
+                for i, exercise_id in enumerate(exercise_ids, 1):
+                    RoutineExercise.objects.filter(
+                        routine=routine,
+                        exercise_id=exercise_id
+                    ).update(order=i)
+            
+            return JsonResponse({'success': True})
+        except Exception as e:
+            return JsonResponse({'success': False, 'error': str(e)})
